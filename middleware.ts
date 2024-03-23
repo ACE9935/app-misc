f6bfbde02983fb7f35eff9f4ff69a6bfac6eaafd
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2024-03-23 04:46:19 +0100

    fix auth

--- middleware.ts
+++ middleware.ts
@@ -5,11 +5,13 @@ import { getToken } from 'next-auth/jwt';
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
     const session = await getToken({ req: request })
-   
+    if (session && request.nextUrl.pathname.startsWith('/signin')) {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
   if(!session) return NextResponse.redirect(new URL('/signin', request.url))
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/profile/:path*', '/favorites'],
+  matcher: ["/signin",'/profile/:path*', '/favorites'],
 }
