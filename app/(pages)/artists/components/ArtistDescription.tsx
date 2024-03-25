283484598ba0d1a775b565ab90f98c94709cb1be
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2024-03-25 01:12:37 +0100

    commit

--- app/(pages)/artists/components/ArtistDescription.tsx
+++ app/(pages)/artists/components/ArtistDescription.tsx
@@ -3,9 +3,9 @@ import { ReactNode } from "react";
 
 function ArtistDescription({children}:{children:ReactNode}) {
     return ( 
-        <div className="p-3 flex flex-col gap-3">
+        <div className="flex flex-col gap-3">
         <MiscTitle>Description</MiscTitle>
-        <div className="rounded-2xl p-6 text-2xl bg-main text-white">
+        <div className="rounded-2xl p-3 sm:p-6 text-xl sm:text-2xl bg-main text-white">
          {children}
         </div>
         </div>
